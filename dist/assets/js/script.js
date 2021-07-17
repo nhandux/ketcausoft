fce0b96a69a92950a7f1cf9ef54e425eb6efae0e
nhandux <nhanduc96@gmail.com>
2021-07-17 11:18:30 +0700

    update main

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -14,6 +14,15 @@ $(".btn-ranger a").on("click", function () {
   $("#" + data_hef).addClass("active show");
   $(".btn-" + data_hef).addClass("active");
 });
+$('.content-item .item h2').on('click', function () {
+  if ($(this).hasClass('active')) {
+    $(this).removeClass('active');
+    $(this).parent().find('.item_list').slideUp();
+  } else {
+    $(this).addClass('active');
+    $(this).parent().find('.item_list').slideDown();
+  }
+});
 $(".list-box-tutorial").on("click", function () {
   $(".list-box-tutorial").removeClass("active");
   $(this).addClass("active");
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,23 +1,33 @@
 $("#loginModal .nav-tabs button").on("click", function () {
-  $("#loginModal .nav-tabs button").removeClass("active");
-  $(this).addClass("active");
-  let data_hef = $(this).attr("data-bs-target");
+    $("#loginModal .nav-tabs button").removeClass("active");
+    $(this).addClass("active");
+    let data_hef = $(this).attr("data-bs-target");
 
-  $("#loginModal .tab-content .tab-pane").removeClass("active show");
-  $(data_hef).addClass("active show");
+    $("#loginModal .tab-content .tab-pane").removeClass("active show");
+    $(data_hef).addClass("active show");
 });
 
 $(".btn-ranger a").on("click", function () {
-  let data_hef = $(this).attr("data-bs-target");
+    let data_hef = $(this).attr("data-bs-target");
 
-  $("#loginModal .nav-tabs button").removeClass("active");
-  $("#loginModal .tab-content .tab-pane").removeClass("active show");
+    $("#loginModal .nav-tabs button").removeClass("active");
+    $("#loginModal .tab-content .tab-pane").removeClass("active show");
 
-  $("#" + data_hef).addClass("active show");
-  $(".btn-" + data_hef).addClass("active");
+    $("#" + data_hef).addClass("active show");
+    $(".btn-" + data_hef).addClass("active");
+});
+
+$('.content-item .item h2').on('click', function () { 
+    if($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        $(this).parent().find('.item_list').slideUp();
+    } else {
+        $(this).addClass('active');
+        $(this).parent().find('.item_list').slideDown();
+    }
 });
 
 $(".list-box-tutorial").on("click", function () {
-  $(".list-box-tutorial").removeClass("active");
-  $(this).addClass("active");
+    $(".list-box-tutorial").removeClass("active");
+    $(this).addClass("active");
 });
