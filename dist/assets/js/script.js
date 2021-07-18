b5d363a9632b8274e59685fc54c548c1867a4a03
Tran Minh Hung <hungtranpro2@enjoyworks.co.kr>
2021-07-18 17:29:43 +0700

    doing task 18/07/2021

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -14,13 +14,13 @@ $(".btn-ranger a").on("click", function () {
   $("#" + data_hef).addClass("active show");
   $(".btn-" + data_hef).addClass("active");
 });
-$('.content-item .item h2').on('click', function () {
-  if ($(this).hasClass('active')) {
-    $(this).removeClass('active');
-    $(this).parent().find('.item_list').slideUp();
+$(".content-item .item h2").on("click", function () {
+  if ($(this).hasClass("active")) {
+    $(this).removeClass("active");
+    $(this).parent().find(".item_list").slideUp();
   } else {
-    $(this).addClass('active');
-    $(this).parent().find('.item_list').slideDown();
+    $(this).addClass("active");
+    $(this).parent().find(".item_list").slideDown();
   }
 });
 $(".list-box-tutorial").on("click", function () {
@@ -39,4 +39,42 @@ $(function () {
 $(".sub-menu-admin").on("click", function (e) {
   console.log(1);
   $(this).toggleClass("active");
+});
+$("#open_change").on("click", function (e) {
+  $("#change_pass").css("display", "flex");
+});
+$("#close_change_pass").on("click", function (e) {
+  $("#change_pass").css("display", "none");
+});
+$(document).ready(function () {
+  $("#show_hide_password_old img").on("click", function (event) {
+    event.preventDefault();
+
+    if ($("#show_hide_password_old input").attr("type") == "text") {
+      $("#show_hide_password_old input").attr("type", "password");
+      $("#show_hide_password_old img").attr("src", "assets/images/eye-close.svg");
+    } else if ($("#show_hide_password_old input").attr("type") == "password") {
+      $("#show_hide_password_old input").attr("type", "text");
+      $("#show_hide_password_old img").attr("src", "assets/images/icon_eye.svg");
+    }
+  });
+  $("#show_hide_password_new img").on("click", function (event) {
+    event.preventDefault();
+
+    if ($("#show_hide_password_new input").attr("type") == "text") {
+      $("#show_hide_password_new input").attr("type", "password");
+      $("#show_hide_password_new img").attr("src", "assets/images/eye-close.svg");
+    } else if ($("#show_hide_password_new input").attr("type") == "password") {
+      $("#show_hide_password_new input").attr("type", "text");
+      $("#show_hide_password_new img").attr("src", "assets/images/icon_eye.svg");
+    }
+  });
+});
+$("#button_substrac").on("click", function (e) {
+  var value = $(".book-page__quantity-input").val();
+  if (value > 1) $(".book-page__quantity-input").val(value - 1);
+});
+$("#button_add").on("click", function (e) {
+  var value = parseInt($(".book-page__quantity-input").val());
+  $(".book-page__quantity-input").val(value + 1);
 });
